c7dd2008c79e752dbf545ee8881174eea1b343af
squinno <57061765+squinno@users.noreply.github.com>
2021-05-16 16:44:08 +1000

    Deleted Lukes files

--- f.m
+++
@@ -1,7 +0,0 @@
-function val = f(s,x)
-h = [];
-for i = 1:size(x,1);
-    h1 = norm(s-x(i,:));
-    h = [h;h1];
-end   
-val =sum(h) + max(h);
--- x_generator.m
+++
@@ -1,9 +0,0 @@
-function val = x_generator(n,min, max)
-rng = 1;
-h = [];
-for i = 1:n;
-
-    h1 = (max-min)*rand(1,2)+min;
-    h = [h;h1];
-end   
-val = h;
