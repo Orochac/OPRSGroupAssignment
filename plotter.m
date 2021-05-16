fc0a6863d9a390fbfe1dd36a6182af62a143d636
AH-Miller <milleralec2000@gmail.com>
2021-05-16 16:24:41 +1000

    Update plotter.m

--- plotter.m
+++ plotter.m
@@ -1,3 +1,4 @@
 [x,y] = meshgrid(-0.5:0.05:1.5,-0.5:0.05:1.5);
 f = sqrt(x.^2 + y.^2) + sqrt((x-1).^2 + y.^2)+ sqrt(x.^2 + (1-y).^2)+max(max(sqrt(x.^2 + y.^2), sqrt((x-1).^2 + y.^2)), sqrt(x.^2 + (1-y).^2));
 surf(f)
+%This is a comment
