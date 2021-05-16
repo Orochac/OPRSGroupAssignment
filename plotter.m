a1b9abfe55eebac10036a4cd57f4a0ef925e03be
Oscar Rochanakij <oscar.rochanakij@student.unimelb.edu.au>
2021-05-16 16:36:00 +1000

    fixed error

--- plotter.m
+++ plotter.m
@@ -1,11 +1,10 @@
 [x,y] = meshgrid(-0.5:0.05:1.5,-0.5:0.05:1.5);
 f = sqrt(x.^2 + y.^2) + sqrt((x-1).^2 + y.^2)+ sqrt(x.^2 + (1-y).^2)+max(max(sqrt(x.^2 + y.^2), sqrt((x-1).^2 + y.^2)), sqrt(x.^2 + (1-y).^2));
 surf(f)
-<<<<<<< HEAD
 
-‘Hello' - Jeff
-=======
+
+%Hello' - Jeff
+
 %This is a comment
 
 %This is a comment written by Luke Quinn
->>>>>>> 7658a0d3e7a34d8e494fb30cbac0c71042358306
