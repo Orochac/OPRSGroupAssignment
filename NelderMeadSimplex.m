ac59208126cd4e22ddcee7fa54e32b631b424cb4
jeffzifanwu <84262576+jeffzifanwu@users.noreply.github.com>
2021-05-17 21:49:21 +1000

    updated nelder mead simplex

--- NelderMeadSimplex.m
+++ NelderMeadSimplex.m
@@ -7,10 +7,14 @@
     % smin: optimal relay location, 1 by 2 matrix
     % fmin: optimal objective value, scalor
     % k: number of iterations
+% Script description:
+    % calls the built in fminsearch function which uses the Nelder-Mead Simplex Method (see link below)
+        % https://au.mathworks.com/help/matlab/math/optimizing-nonlinear-functions.html#bsgpq6p-11
 % version history:
     % JW,  2021/05/17, Created for MAST30013 Project
 %% Function
 function [smin, fmin, k] = NelderMeadSimplex(s0, nmbrSensors, sensorLocations)
-    [smin, fmin] = fminsearch(@P, s0)
+    f = @(x)P(x, nmbrSensors, sensorLocations) % create a new objective function which is parameterised only on s0
+    [smin, fmin] = fminsearch(f, s0)
     k=0 % unknown because it's a matlab built in function
 end
