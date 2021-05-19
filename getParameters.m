460d4a77a202dfb7ed6bd8c6018fdb7718827813
jeffzifanwu <84262576+jeffzifanwu@users.noreply.github.com>
2021-05-20 02:48:44 +1000

    Update getParameters.m

--- getParameters.m
+++ getParameters.m
@@ -5,11 +5,12 @@
     % JW, Created for MAST30013 Project, 2021/05/15
 %% User Defined Parameters
 nmbrSimPerInitialRelayScenario = 10; % number of simulations per initial relay scenario
-nmbrSensorSets = 3;
-nmbrAlgorithms = 3;
 sensorQuantity = [3, 6, 12, 24, 48, 96, 192, 384];
+plotFigureInd1 = false; % indicator for whether or not to plot Initial Relay And Sensor Locations, images will be save in folder: figuresInitialRelayAndSensorLoc
 %% System parameters
 % system parameters should only be modified by developers of the main procedures
 nmbrInitialRelayScenario = 4; % developer must match this number with the number of cases in generateInitialRelay.m
+nmbrSensorSets = 3; % developer must match this number with the number of cases in generateSensorSet.m
+nmbrAlgorithms = 5; % developer must match this number with the number of algorithm cases in the mainProcedures.m
 %% Calculated Parameters
 nmbrRelayInitialLoc = nmbrSimPerInitialRelayScenario * nmbrInitialRelayScenario;
